8e78c1f853aa06f87f4abf306ea963cc8c7aca54
ENOC NARVAEZ <enocnarvaez@unan.edu.ni>
2019-11-10 04:27:11 -0600

    Registro e Inicio de Session desde firebase

--- sitioweb/js/registro.js
+++ sitioweb/js/registro.js
@@ -3,20 +3,32 @@ function registrar()
     var email=document.getElementById('email').value;
     var pass=document.getElementById('pass').value;
 
-    firebase.auth().createUserWithEmailAndPassword(email, pass)
-    .catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  if (errorCode == 'auth/weak-password') {
-    alert('The password is too weak.');
-  } else {
-    alert(errorMessage);
+                firebase.auth().createUserWithEmailAndPassword(email, pass)
+                .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+            });
+
+
+            
+  function ingreso()
+  {
+    var email2=document.getElementById('email').value;
+    var pass2=document.getElementById('pass').value;
+    firebase.auth().signInWithEmailAndPassword(email2, pass2)
+    .then(function(result) {
+      // result.user.tenantId should be ‘TENANT_PROJECT_ID’.
+    }).catch(function(error) {
+      // Handle error.
+    });
   }
-  console.log(error);
-});
-   alert(email);
-   alert(pass);
    
 
 }
